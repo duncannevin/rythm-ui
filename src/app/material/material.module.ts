8eec10f8d050e4fbea7863a8e7ee1dd6e44f26e1
Duncan Nevin <duncan.nevin@gmail.com>
2020-06-08 15:05:04 -0400

    WIP: sets some themes

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
 	declarations: [],
 	imports: [CommonModule],
-	exports: [MatToolbarModule],
+	exports: [MatToolbarModule, MatButtonModule],
 })
 export class MaterialModule {}
